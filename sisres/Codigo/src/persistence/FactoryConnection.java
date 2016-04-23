5965f44152f78a832c8ae292c60b90933cd62aae
VinyPinheiro <viny-pinheiro@hotmail.com>
2016-04-23 12:05:33 -0300

    Add assert and consts into FactoryConnection

--- sisres/Codigo/src/persistence/FactoryConnection.java
+++ sisres/Codigo/src/persistence/FactoryConnection.java
@@ -8,9 +8,9 @@ import java.sql.SQLException;
 public class FactoryConnection {
 	static String statusConnection = "";
 	
-	private String local = "jdbc:mysql://localhost/sisres_db";
-	private String user = "testuser";
-	private String password = "password";
+	private final String LOCAL = "jdbc:mysql://localhost/sisres_db";
+	private final String USER = "testuser";
+	private final String PASSWORD = "password";
 	
 	//Singleton
 		private static FactoryConnection instance;
@@ -26,7 +26,9 @@ public class FactoryConnection {
 		
 	public Connection getConnection() throws SQLException{
 		Connection con = null;
-		con = DriverManager.getConnection(local, user, password);
+		con = DriverManager.getConnection(LOCAL, USER, PASSWORD);
+		assert con != null;
+		
 		return con;
 	}
 
