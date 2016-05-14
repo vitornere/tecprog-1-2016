de1718d1179377af9079d7e95b3c7c25111e0440
Antonio Carvalho <_antonio_@hotmail.com>
2016-05-14 17:33:35 -0300

    Uncomplicate class ClassroomRegistration.java

--- sisres/Codigo/src/view/cadastros/ClassroomRegistration.java
+++ sisres/Codigo/src/view/cadastros/ClassroomRegistration.java
@@ -9,30 +9,30 @@ import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
 import control.ManterSala;
-import exception.PatrimonioException;
+import exception.PatrimonyException;
 
 /**
  * 
  * @author Parley
  */
-public class CadastroSala extends CadastroPatrimonio {
+public class ClassroomRegistration extends PatrimonyRegistration {
 
-    public CadastroSala(java.awt.Frame parent, boolean modal) {
+    public ClassroomRegistration(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         this.setName("CadastroSala");
     }
 
-    @Override protected void cadastroAction() {
+    @Override protected void registrationAction() {
         try {
             // JOptionPane.showMessageDialog(this, codigoTxtField.getText() +
             // descricaoTextArea.getText() + capacidadeTxtField.getText(),
             // "teste", JOptionPane.INFORMATION_MESSAGE, null);
-            ManterSala.getInstance().inserir(codigoTxtField.getText(), descricaoTextArea.getText(), capacidadeTxtField.getText());
+            ManterSala.getInstance().inserir(codeTxtField.getText(), discriptionTextArea.getText(), capacityTxtField.getText());
 
             JOptionPane.showMessageDialog(this, "Sala Cadastrada com sucesso", "Sucesso", JOptionPane.INFORMATION_MESSAGE, null);
             this.setVisible(false);
 
-        } catch (PatrimonioException ex) {
+        } catch (PatrimonyException ex) {
             JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(this, ex.getSQLState() + "\n" + ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
