94b1f0e099af59f8688df30cf60ef17c226e70f2
Hugo Martins <hugomartins_15@hotmail.com>
2016-05-17 16:25:33 -0300

    Uncomplicate interfaces package Classes drugstore system

--- drogaria/src/interfaces/Recommended.java
+++ drogaria/src/interfaces/Recommended.java
@@ -2,6 +2,5 @@ package interfaces;
 
 public interface Recommended {
 
-	// Assinatura do método postarRecomendações
 	public void medicineRecommended(String typeMedicine, String use);
 }
--- drogaria/src/interfaces/Test.java
+++ drogaria/src/interfaces/Test.java
@@ -1,14 +1,17 @@
 package interfaces;
 
-import entidades.Cliente;
-	//Testando a Interface
-	public class Test {
-		public static void main(String[] args) {
-			Cliente newClient = new Cliente("13.999.888-DF","444.555.666", 11, "Teste", "Teste2" , "Gama qd. 1000", "(61)-1234-5678","cliente1@gmail.com");
-		    newClient.medicineRecommended("GENERICO", "PEDIATRICO");
-		    //Lembrando que pode alterar os tipos de remédios, que são: "TARJA PRETA" e "GENÉRICO"
-		    //Também vale o mesmo para o uso: "ADULTO" e "PEDIÁTRICO"
-		}
+import entities.Client;
+
+public class Test {
+	public static void main(String[] args) {
+		Client newClient = new Client("13.999.888-DF", "444.555.666", 11,
+				"Test", "Test2", "Gama qd. 1000", "(61)-1234-5678",
+				"cliente1@gmail.com");
+		newClient.medicineRecommended("GENERIC", "PEDIATRIC");
+		// Recalling that can change the types of remedies that are
+		// "BLACK TARGE" and "GENERIC"
+		// It is also worth it for use: "ADULT" and "PEDIATRIC"
+	}
 
 }
 
