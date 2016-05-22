eb0b3f8e36c17a8fa3e64bacd88fa22944dd18c3
Antonio Carvalho <_antonio_@hotmail.com>
2016-05-21 23:51:51 -0300

    Commenting and change things in the view/alteracoes

--- sisres/Codigo/src/view/alteracoes/ChangeProfessor.java
+++ sisres/Codigo/src/view/alteracoes/ChangeProfessor.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package view.alteracoes;
 
 import java.sql.SQLException;
@@ -12,47 +8,44 @@ import view.cadastros.ClientRegistration;
 import control.ManterProfessor;
 import exception.ClienteException;
 
-/**
- * 
- * @author Parley
- */
 public class ChangeProfessor extends ClientRegistration {
 
-    int index2 = 0;
+	int index2 = 0;
 
-    public ChangeProfessor(java.awt.Frame parent, boolean modal, int index) {
-        super(parent, modal);
-        this.setName("AlterarProfessor");
-        this.btnRegistration.setText("Alterar");
-        this.btnRegistration.setName("Alterar");
-        this.index2 = index;
+	public ChangeProfessor(java.awt.Frame parent, boolean modal, int index) {
+		super(parent, modal);
+		this.setName("AlterarProfessor");
+		this.btnRegistration.setText("Alterar");
+		this.btnRegistration.setName("Alterar");
+		this.index2 = index;
 
-        try {
-            this.nameTxtField.setText(ManterProfessor.getInstance().getProfessores_vet().get(index).getNome());
-            this.emailTxtField.setText(ManterProfessor.getInstance().getProfessores_vet().get(index).getEmail());
-            this.phoneTxtField.setText(ManterProfessor.getInstance().getProfessores_vet().get(index).getTelefone());
-            this.enrollmentTxtField.setText(ManterProfessor.getInstance().getProfessores_vet().get(index).getMatricula());
-            this.cpfTxtField.setText(ManterProfessor.getInstance().getProfessores_vet().get(index).getCpf());
+		try {
+			this.nameTxtField.setText(ManterProfessor.getInstance().getProfessores_vet().get(index).getNome());
+			this.emailTxtField.setText(ManterProfessor.getInstance().getProfessores_vet().get(index).getEmail());
+			this.phoneTxtField.setText(ManterProfessor.getInstance().getProfessores_vet().get(index).getTelefone());
+			this.enrollmentTxtField.setText(ManterProfessor.getInstance().getProfessores_vet().get(index).getMatricula());
+			this.cpfTxtField.setText(ManterProfessor.getInstance().getProfessores_vet().get(index).getCpf());
 
-        } catch (ClienteException ex) {
-            JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
-        }
-    }
+		} catch (ClienteException ex) {
+			JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
+		} catch (SQLException ex) {
+			JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
+		}
+	}
 
-    @Override public void registrationAction() {
-        try {
-            ManterProfessor.getInstance().alterar(nameTxtField.getText(), cpfTxtField.getText(), enrollmentTxtField.getText(),
-                    phoneTxtField.getText(), emailTxtField.getText(),
-                    ManterProfessor.getInstance().getProfessores_vet().get(index2));
+	public void registrationAction() {
+		try {
+			ManterProfessor.getInstance().alterar(nameTxtField.getText(), cpfTxtField.getText(),
+					enrollmentTxtField.getText(), phoneTxtField.getText(), emailTxtField.getText(),
+					ManterProfessor.getInstance().getProfessores_vet().get(index2));
 
-            JOptionPane.showMessageDialog(this, "Cadastro alterado com sucesso", "Sucesso", JOptionPane.INFORMATION_MESSAGE, null);
-            this.setVisible(false);
-        } catch (ClienteException ex) {
-            JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
-        }
-    }
+			JOptionPane.showMessageDialog(this, "Cadastro alterado com sucesso", "Sucesso",
+					JOptionPane.INFORMATION_MESSAGE, null);
+			this.setVisible(false);
+		} catch (ClienteException ex) {
+			JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
+		} catch (SQLException ex) {
+			JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
+		}
+	}
 }
