f6c4369e59059e4d5c6f8512148c285981bb3744
Antonio Carvalho <_antonio_@hotmail.com>
2016-06-02 00:14:32 -0300

    Commenting methods and creating header in EquipmentRegistration.java

--- sisres/Codigo/src/view/cadastros/EquipmentRegistration.java
+++ sisres/Codigo/src/view/cadastros/EquipmentRegistration.java
@@ -1,3 +1,8 @@
+/** 
+ * Name:EquipmentRegistration.java
+ * Class is a sub class for adding a new equipment.
+ */
+
 package view.cadastros;
 
 import java.sql.SQLException;
@@ -9,28 +14,40 @@ import exception.PatrimonyException;
 
 public class EquipmentRegistration extends PatrimonyRegistration {
 
-    public EquipmentRegistration(java.awt.Frame parent, boolean modal) {
-        super(parent, modal);
-        this.setName("CadastroEquipamento");
-        this.lblCapacity.setVisible(false);
-        this.capacityTxtField.setVisible(false);
-    }
+	/**
+	 * Method to generate the option screen to add equipment.
+	 * 
+	 * @param parent
+	 * @param modal
+	 */
+	
+	public EquipmentRegistration(java.awt.Frame parent, boolean modal) {
+		super(parent, modal);
+		this.setName("CadastroEquipamento");
+		this.lblCapacity.setVisible(false);
+		this.capacityTxtField.setVisible(false);
+	}
 
-   protected void registrationAction() {
+	/**
+	 * Method to generate the screen to add information from a new equipment and advises that 
+	 * was successfully changed
+	 */
+	
+	protected void registrationAction() {
 
-        try {
-            ManterEquipamento.getInstance().inserir(codeTxtField.getText(), discriptionTextArea.getText());
-            JOptionPane.showMessageDialog(this, "Equipamento Cadastrado com sucesso", "Sucesso", JOptionPane.INFORMATION_MESSAGE,
-                    null);
-            this.setVisible(false);
+		try {
+			ManterEquipamento.getInstance().inserir(codeTxtField.getText(), discriptionTextArea.getText());
+			JOptionPane.showMessageDialog(this, "Equipamento Cadastrado com sucesso", "Sucesso",
+					JOptionPane.INFORMATION_MESSAGE, null);
+			this.setVisible(false);
 
-        } catch (PatrimonyException ex) {
-            JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
-        } catch (NullPointerException ex) {
-            JOptionPane.showMessageDialog(this, ex.getLocalizedMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
-        }
+		} catch (PatrimonyException ex) {
+			JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
+		} catch (SQLException ex) {
+			JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
+		} catch (NullPointerException ex) {
+			JOptionPane.showMessageDialog(this, ex.getLocalizedMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
+		}
 
-    }
+	}
 }
