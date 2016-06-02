01f2de8eef23e72281cc85189e22d66def87d532
Antonio Carvalho <_antonio_@hotmail.com>
2016-06-02 00:15:58 -0300

    Commenting methods and creating header in StudentRegistration.java

--- sisres/Codigo/src/view/cadastros/StudentRegistration.java
+++ sisres/Codigo/src/view/cadastros/StudentRegistration.java
@@ -1,3 +1,8 @@
+/**
+ * Name:StudentRegistration.java
+ * Class is a sub class for adding a new student.
+ */
+
 package view.cadastros;
 
 import java.sql.SQLException;
@@ -9,28 +14,40 @@ import exception.ClienteException;
 
 public class StudentRegistration extends ClientRegistration {
 
-    public StudentRegistration(java.awt.Frame parent, boolean modal) {
-        super(parent, modal);
-        this.setName("CadastroAluno");
-    }
+	/**
+	 * Method to generate the option screen to add student.
+	 * 
+	 * @param parent
+	 * @param modal
+	 */
+	
+	public StudentRegistration(java.awt.Frame parent, boolean modal) {
+		super(parent, modal);
+		this.setName("CadastroAluno");
+	}
 
-   public void registrationAction() {
-        try {
-            if (btnRegistration.getText().equals("Cadastrar")) {
-                ManterAluno.getInstance().inserir(nameTxtField.getText(), cpfTxtField.getText(), enrollmentTxtField.getText(),
-                        phoneTxtField.getText(), emailTxtField.getText());
+	/**
+	 * Method to generate the screen to add information from a new student and advises that 
+	 * was successfully changed
+	 */
+	
+	public void registrationAction() {
+		try {
+			if (btnRegistration.getText().equals("Cadastrar")) {
+				ManterAluno.getInstance().inserir(nameTxtField.getText(), cpfTxtField.getText(),
+						enrollmentTxtField.getText(), phoneTxtField.getText(), emailTxtField.getText());
 
-                JOptionPane.showMessageDialog(this, "Aluno Cadastrado com sucesso", "Sucesso", JOptionPane.INFORMATION_MESSAGE,
-                        null);
+				JOptionPane.showMessageDialog(this, "Aluno Cadastrado com sucesso", "Sucesso",
+						JOptionPane.INFORMATION_MESSAGE, null);
 
-                this.setVisible(false);
-            }
-        } catch (ClienteException ex) {
-            JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
-        } catch (NullPointerException ex) {
-            JOptionPane.showMessageDialog(this, ex.getLocalizedMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
-        }
-    }
+				this.setVisible(false);
+			}
+		} catch (ClienteException ex) {
+			JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
+		} catch (SQLException ex) {
+			JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
+		} catch (NullPointerException ex) {
+			JOptionPane.showMessageDialog(this, ex.getLocalizedMessage(), "Erro", JOptionPane.ERROR_MESSAGE, null);
+		}
+	}
 }
