303fcc6d3cc12ebd9a4f91a6dc20f58957bbfe88
Vitor Nere <vitor.nere@hotmail.com>
2016-06-19 21:38:41 -0300

    Fix tests of sisres/Codigo/unitary_tests/test/model/ReserveClassroomForStudentTest.java

--- sisres/Codigo/unitary_tests/test/ReservaSalaAlunoSuite.java
+++ sisres/Codigo/unitary_tests/test/ReservaSalaAlunoSuite.java
@@ -5,11 +5,11 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import test.control.TestReserveClassroomForStudentPersist;
-import test.model.ResSalaAlunoTest;
+import test.model.ReserveClassroomForStudentTest;
 import test.persistence.ResSalaAlunoDAOTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ResSalaAlunoTest.class, ResSalaAlunoDAOTest.class, TestReserveClassroomForStudentPersist.class})
+@SuiteClasses({ReserveClassroomForStudentTest.class, ResSalaAlunoDAOTest.class, TestReserveClassroomForStudentPersist.class})
 public class ReservaSalaAlunoSuite {
 
 }
