514ff072cef8d81c1aeedeea8dd1418295b90d4b
Antonio Carvalho <_antonio_@hotmail.com>
2016-06-27 16:22:34 -0300

    Fixing problems in TestCashier.java

--- drogaria/Tests/tests/TestCashier.java
+++ drogaria/Tests/tests/TestCashier.java
@@ -26,20 +26,20 @@ public class TestCashier{
 	@Test
 	public void testGetTipoo(){
 		Cashier transacao = new Cashier ("Teste 1","Teste 2", 50, "Teste 3", "Teste 4", "Teste 5", "Teste 6", 0, 100, "10/10/2020", "transacaoTeste",1);
-		assertEquals(transacao.getTransationType() ,0);
+		assertEquals(transacao.getType() ,0);
 	}
 	
 	
 	@Test
 	public void testGetValor (){
 		Cashier transacao = new Cashier ("Teste 1","Teste 2", 50, "Teste 3", "Teste 4", "Teste 5", "Teste 6", 0, 100, "10/10/2020", "transacaoTeste",1);
-		assertEquals(transacao.getTransactionValue(),100, 0.1);
+		assertEquals(transacao.getValue(),100, 0.1);
 	}
 	
 	@Test
 	public void testGetData (){
 		Cashier transacao = new Cashier ("Teste 1","Teste 2", 50, "Teste 3", "Teste 4", "Teste 5", "Teste 6", 0, 100, "10/10/2020", "transacaoTeste",1);
-		assertEquals(transacao.getTransationDate(),"10/10/2020");
+		assertEquals(transacao.getDate(),"10/10/2020");
 	}
 	
 	@Test
