71685122da39a41d07a6e0f02acb87eb83352cb3
Antonio Carvalho <_antonio_@hotmail.com>
2016-06-29 23:25:59 -0300

    Adding more tests in TestCashier.java

--- drogaria/Tests/tests/TestCashier.java
+++ drogaria/Tests/tests/TestCashier.java
@@ -25,10 +25,12 @@ public class TestCashier{
 		assertEquals(transacao.getValue(),100, 0.1);
 	}
 	
+
+	
 	@Test
-	public void GetDateTest (){
+	public void GetCodeTest (){
 		Cashier transacao = new Cashier ("Teste 1","Teste 2", 50, "Teste 3", "Teste 4", "Teste 5", "Teste 6", 0, 100, "10/10/2020", "transacaoTeste",1);
-		assertEquals(transacao.getDate(),"10/10/2020");
+		assertEquals(transacao.getCode(),1);
 	}
 	
 	@Test
@@ -65,4 +67,13 @@ public class TestCashier{
 		cashier.setCurrentBalance(100);
 		assertEquals(100, cashier.getCurrentBalance(), 0.1);
 	}
+	
+	@Test
+	public void depositTest(){
+		Cashier cashier = new Cashier();
+		cashier.setCurrentBalance(100);
+		cashier.deposit(20);
+		assertEquals(120, cashier.getCurrentBalance(), 0.1);
+	}
+	
 }
