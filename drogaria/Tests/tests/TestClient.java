042d73ba4131aa048470525ec8869840517d3c8f
Hugo Martins <hugomartins_15@hotmail.com>
2016-07-04 03:33:09 -0300

    Adding Test Unit to Client

--- drogaria/Tests/tests/TestClient.java
+++ drogaria/Tests/tests/TestClient.java
@@ -18,16 +18,113 @@ public class TestClient {
 	
 
 	@Test
+	public void testPersonIdentity() {
+		Client newClient = new Client("personIdentity","Test 2", 50, "Test 3", "Test 4", "Test 5", "Test 6");
+		assertEquals(newClient.getIdentity(),"personIdentity");
+		
+	}
+	
+	@Test
+	public void testPersonIdentityInvalid() {
+		Client newClient = new Client("personIdentity","Test 2", 50, "Test 3", "Test 4", "Test 5", "Test 6");
+		assertNotEquals(newClient.getIdentity(),"personIdentityInvalid");
+		
+	}
+	
+	@Test
+	public void testPersonCpf() {
+		Client newClient = new Client("personIdentity","personCpf", 50, "Test 3", "Test 4", "Test 5", "Test 6");
+		assertEquals(newClient.getCpfPerson(),"personCpf");
+		
+	}
+	
+	@Test
+	public void testPersonCpfInvalid() {
+		Client newClient = new Client("personIdentity","personCpf", 50, "Test 3", "Test 4", "Test 5", "Test 6");
+		assertNotEquals(newClient.getCpfPerson(),"personCpfInvalid");
+		
+	}
+	
+	@Test
 	public void testSetDigitoCpf() {
 		Client newClient = new Client("Test 1","Test 2", 50, "Test 3", "Test 4", "Test 5", "Test 6");
 		assertEquals(newClient.getDigitCpfPerson(),50);
 		
 	}
+	
+	@Test
+	public void testSetDigitoCpfInvalid() {
+		Client newClient = new Client("Test 1","Test 2", 50, "Test 3", "Test 4", "Test 5", "Test 6");
+		assertNotEquals(newClient.getDigitCpfPerson(),51);
+		
+	}
+	
+	@Test
+	public void testSetPersonName() {
+		Client newClient = new Client("Test 1","Test 2", 50, "personName", "Test 4", "Test 5", "Test 6");
+		assertEquals(newClient.getName(),"personName");
+		
+	}
+	
+	@Test
+	public void testSetPersonNameInvalid() {
+		Client newClient = new Client("Test 1","Test 2", 50, "personName", "Test 4", "Test 5", "Test 6");
+		assertNotEquals(newClient.getName(),"personNameInvalid");
+		
+	}
+	
+	@Test
+	public void testSetPastName() {
+		Client newClient = new Client("Test 1","Test 2", 50, "personName", "pastName", "Test 5", "Test 6");
+		assertEquals(newClient.getPastName(),"pastName");
+		
+	}
+	
+	@Test
+	public void testSetPastNameInvalid() {
+		Client newClient = new Client("Test 1","Test 2", 50, "personName", "pastName", "Test 5", "Test 6");
+		assertNotEquals(newClient.getPastName(),"pastNameInvalid");
+		
+	}
 
+	@Test
+	public void testSetAddress() {
+		Client newClient = new Client("Test 1","Test 2", 50, "personName", "pastName", "address", "Test 6");
+		assertEquals(newClient.getAddress(),"address");
+		
+	}
+	
+	@Test
+	public void testSetAddressInvalid() {
+		Client newClient = new Client("Test 1","Test 2", 50, "personName", "pastName", "address", "Test 6");
+		assertNotEquals(newClient.getAddress(),"pastNameInvalid");
+		
+	}
+	
+	@Test
+	public void testSetPhone() {
+		Client newClient = new Client("Test 1","Test 2", 50, "personName", "pastName", "address", "phone");
+		assertEquals(newClient.getPhone(),"phone");
+		
+	}
+	
+	@Test
+	public void testSetPhoneInvalid() {
+		Client newClient = new Client("Test 1","Test 2", 50, "personName", "pastName", "address", "phone");
+		assertNotEquals(newClient.getPhone(),"phoneInvalid");
+		
+	}
+	
 	@Test
 	public void testEmail(){
 		Client newClient = new Client("Test 1","Test 2", 50, "Test 3", "Test 4", "Test 5", "Test 6","email@email.com");
-		assertEquals("email@email.com", newClient.getEmail());
+		assertEquals(newClient.getEmail(), "email@email.com");
+	}
+	
+	@Test
+	public void testEmailInvalid(){
+		Client newClient = new Client("Test 1","Test 2", 50, "Test 3", "Test 4", "Test 5", "Test 6","email@email.com");
+		assertNotEquals(newClient.getEmail(), "emailInvalid@email.com");
 	}
 	
 	@Test
